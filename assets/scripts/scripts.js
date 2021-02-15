c0067f08c6f929f9a58b142007647d8bad702f14
Conkeegs <conkeegs@gmail.com>
2021-02-15 02:37:17 -0500

    pacman movement

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -2,3 +2,32 @@ let pacManBody = document.createElement("div");
 pacManBody.textContent = " ";
 pacManBody.id = "pacManBody";
 document.body.appendChild(pacManBody);
+
+let position = {
+
+    "x": pacManBody.getBoundingClientRect().x,
+    "y": pacManBody.getBoundingClientRect().y
+
+};
+
+function movePlayer(event) {
+
+    switch (event.key) {
+
+        case "ArrowLeft":
+
+            position.x -= 10;
+            pacManBody.style.left = position.x + "px";
+            break;
+
+        case "ArrowRight":
+
+            position.x += 10;
+            pacManBody.style.left = position.x + "px";
+            break;
+
+    }
+
+}
+
+window.addEventListener("keydown", movePlayer);
