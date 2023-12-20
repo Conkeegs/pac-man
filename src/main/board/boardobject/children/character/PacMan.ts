30161276439b9864e3022e8f4e5006412e7dc147
Conkeegs <conkeegs@gmail.com>
2023-12-20 11:10:54 -0500

    changed function in pacman to private

--- src/main/board/boardobject/children/character/PacMan.ts
+++ src/main/board/boardobject/children/character/PacMan.ts
@@ -1,40 +1,40 @@
-'use strict';
+"use strict";
 
 import { exists } from "../../../../utils/Utils";
 import Character from "./Character";
 import MovementDirection from "./MovementDirection";
 
 export default class PacMan extends Character {
-    private lastMoveCode: MovementDirection | null = null;
-    private moveCodes = {
-        ArrowLeft: MovementDirection.LEFT,
-        ArrowRight: MovementDirection.RIGHT,
-        ArrowUp: MovementDirection.UP,
-        ArrowDown: MovementDirection.DOWN,
-        Space: MovementDirection.STOP
-    };
+	private lastMoveCode: MovementDirection | null = null;
+	private moveCodes = {
+		ArrowLeft: MovementDirection.LEFT,
+		ArrowRight: MovementDirection.RIGHT,
+		ArrowUp: MovementDirection.UP,
+		ArrowDown: MovementDirection.DOWN,
+		Space: MovementDirection.STOP,
+	};
 
-    constructor(name: string, source: string) {
-        super(name, source);
+	constructor(name: string, speed: number, source: string) {
+		super(name, speed, source);
 
-        this.#createMoveEventListeners();
-    }
+		this.createMoveEventListeners();
+	}
 
-    #createMoveEventListeners() {
-        document.body.addEventListener('keydown', (event) => {
-            event.stopImmediatePropagation();
+	private createMoveEventListeners() {
+		document.body.addEventListener("keydown", (event) => {
+			event.stopImmediatePropagation();
 
-            let moveCode = this.moveCodes[event.code as keyof typeof this.moveCodes];
+			let moveCode = this.moveCodes[event.code as keyof typeof this.moveCodes];
 
-            if (moveCode == 4 && this.isMoving()) {
-                this.stopMoving();
-                return;
-            }
+			if (moveCode == 4 && this.isMoving()) {
+				this.stopMoving();
+				return;
+			}
 
-            if (exists(moveCode) && (!this.isMoving() || this.lastMoveCode !== moveCode)) {
-                this.startMoving(moveCode);
-                this.lastMoveCode = moveCode;
-            }
-        });
-    }
+			if (exists(moveCode) && (!this.isMoving() || this.lastMoveCode !== moveCode)) {
+				this.startMoving(moveCode);
+				this.lastMoveCode = moveCode;
+			}
+		});
+	}
 }
