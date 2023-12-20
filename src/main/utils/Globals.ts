db3f95d470a475ff229fad1077a7e447b7d62210
Conkeegs <conkeegs@gmail.com>
2023-12-20 12:59:08 -0500

    global typings update

--- src/main/utils/Globals.ts
+++ src/main/utils/Globals.ts
@@ -1,13 +1,13 @@
-'use strict';
+"use strict";
 
-export const COLUMNS: number = 28;
-export const ROWS: number = 36;
+export const COLUMNS: 28 = 28;
+export const ROWS: 36 = 36;
 
 export const DEBUG: boolean = false;
 
 export const GAMEOBJECTS: string[] = [];
 
-let match = window.matchMedia('(min-width: 720px)');
+let match = window.matchMedia("(min-width: 720px)");
 
 export const HEIGHT: number = match.matches ? 864 : 576;
 export const WIDTH: number = match.matches ? 672 : 448;
