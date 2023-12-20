888049d00996d3da51ba5461f20358931eb2bc96
Conkeegs <conkeegs@gmail.com>
2023-12-20 10:52:11 -0500

    boardtext formatting

--- src/main/board/boardobject/children/BoardText.ts
+++ src/main/board/boardobject/children/BoardText.ts
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 import DebugWindow from "../../../debugwindow/DebugWindow";
 import { TILESIZE } from "../../../utils/Globals";
@@ -6,21 +6,23 @@ import { px } from "../../../utils/Utils";
 import { BoardObject } from "../BoardObject";
 
 export default class BoardText extends BoardObject {
-    public override width = TILESIZE;
-    public override height = TILESIZE;
+	public override width = TILESIZE;
+	public override height = TILESIZE;
 
-    constructor(name: string, text: string, fontsize = TILESIZE, color = 'white') {
-        super(name);
+	constructor(name: string, text: string, fontsize = TILESIZE, color = "white") {
+		super(name);
 
-        if (fontsize > 24) {
-            DebugWindow.error('BoardText.js', 'constructor', `fontsize cannot be greater than ${TILESIZE}.`);
-        }
+		if (fontsize > 24) {
+			DebugWindow.error("BoardText.js", "constructor", `fontsize cannot be greater than ${TILESIZE}.`);
+		}
 
-        (this.getElement().css({
-            width: px(this.width),
-            height: px(this.height),
-            fontSize: px(fontsize),
-            color: color,
-        } as CSSStyleDeclaration) as HTMLElement).textContent = text;
-    }
+		(
+			this.getElement().css({
+				width: px(this.width),
+				height: px(this.height),
+				fontSize: px(fontsize),
+				color: color,
+			} as CSSStyleDeclaration) as HTMLElement
+		).textContent = text;
+	}
 }
