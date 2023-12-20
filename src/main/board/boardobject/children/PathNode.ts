cfc8fb3ca6fc089a5d494180fb174e562ae1b1da
Conkeegs <conkeegs@gmail.com>
2023-12-20 10:52:26 -0500

    pathnode formatting

--- src/main/board/boardobject/children/PathNode.ts
+++ src/main/board/boardobject/children/PathNode.ts
@@ -1,27 +1,29 @@
-'use strict';
+"use strict";
 
 import { TILESIZE } from "../../../utils/Globals";
 import { create, px } from "../../../utils/Utils";
 import { BoardObject } from "../BoardObject";
 
 export default class PathNode extends BoardObject {
-    public override width = TILESIZE;
-    public override height = TILESIZE;
+	public override width = TILESIZE;
+	public override height = TILESIZE;
 
-    constructor(name: string, color = 'white') {
-        super(name);
+	constructor(name: string, color = "white") {
+		super(name);
 
-        const element: HTMLElement = this.getElement();
+		const element: HTMLElement = this.getElement();
 
-        element.css({
-            width: px(this.width),
-            height: px(this.height),
-        } as CSSStyleDeclaration);
+		element.css({
+			width: px(this.width),
+			height: px(this.height),
+		} as CSSStyleDeclaration);
 
-        element.appendChild(create('div', `${name}-node-element`, ['node']).css({
-            width: px(TILESIZE * 0.5),
-            height: px(TILESIZE * 0.5),
-            backgroundColor: color
-        } as CSSStyleDeclaration) as HTMLElement);
-    }
+		element.appendChild(
+			create("div", `${name}-node-element`, ["node"]).css({
+				width: px(TILESIZE * 0.5),
+				height: px(TILESIZE * 0.5),
+				backgroundColor: color,
+			} as CSSStyleDeclaration) as HTMLElement
+		);
+	}
 }
