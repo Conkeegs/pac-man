cc17d7d934152a2b353169c2081b7a507502a653
Conkeegs <conkeegs@gmail.com>
2024-01-07 13:46:06 -0500

    config file updates

--- spack.config.js
+++ spack.config.js
@@ -1,10 +1,65 @@
 const { config } = require("@swc/core/spack");
+const path = require("path");
 
 module.exports = config({
-    entry: {
-        web: __dirname + "/src/main/App.ts",
-    },
-    output: {
-        path: __dirname + "/lib",
-    }
+	target: "browser",
+	entry: {
+		web: path.join(__dirname, "dist", "main", "App.js"),
+	},
+	output: {
+		path: path.join(__dirname, "dist"),
+	},
+	module: {
+		type: "es6",
+		strict: false,
+		strictMode: true,
+		lazy: false,
+		noInterop: false,
+	},
+	options: {
+		test: ".*\\.ts$",
+		sourceMaps: "inline",
+		// inlineSourcesContent: true,
+		// isModule: true,
+		exclude: [
+			// "**/*.spec.ts",
+			"node_modules",
+		],
+		env: {
+			loose: true,
+			bugfixes: true,
+		},
+		// minify: true,
+		// script: true,
+		jsc: {
+			// "minify": {
+			//     "compress": true,
+			//     "dead_code": true
+			// },
+			// "baseUrl": "./src",
+			parser: {
+				syntax: "typescript",
+				tsx: false,
+				dynamicImport: false,
+				decorators: false,
+			},
+			target: "es2015",
+			loose: true,
+			externalHelpers: true,
+			keepClassNames: false,
+			transform: {
+				constModules: {
+					globals: {
+						"@pacman/env-flags": {
+							DEBUG: "true",
+						},
+						"@pacman/features": {
+							FEATURE_A: "false",
+							FEATURE_B: "true",
+						},
+					},
+				},
+			},
+		},
+	},
 });
