ff6036cc6c6cd00ce22e831c9ef1344beacea706
Conkeegs <conkeegs@gmail.com>
2024-03-01 20:53:34 -0500

    debugwindow notes

--- src/main/debugwindow/DebugWindow.ts
+++ src/main/debugwindow/DebugWindow.ts
@@ -3,6 +3,22 @@
 import { DEBUG } from "../utils/Globals.js";
 import { get } from "../utils/Utils.js";
 
+// positionSum = 0;
+// positionCount = 0;
+// frameSecondsSum = 0;
+// frameCount = 0; properties on character class for framerate
+
+// this.positionSum += this.speed! * millisToSeconds(timeStamp - lastAnimationTime);
+// this.positionCount++;
+// this.frameSecondsSum += millisToSeconds(timeStamp - lastAnimationTime); in if (lastAnimationTime) in Character class and "// this.frameCount++;" after setting lastAnimationTime
+
+// private moveCodeWords = {
+// 	[MovementDirection.LEFT]: "LEFT",
+// 	[MovementDirection.RIGHT]: "RIGHT",
+// 	[MovementDirection.UP]: "UP",
+// 	[MovementDirection.DOWN]: "DOWN",
+// };
+
 /**
  * Represents the debug window that shows up next to the board when the `DEBUG` global flag is set.
  * It logs important information about the game.
