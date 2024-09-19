a0c0bbaf520e73dce690fbcab21f27cf74e9d576
Conkeegs <conkeegs@gmail.com>
2024-09-18 20:19:19 -0400

    more uielement logic added and initial logic for displaying modals created

--- src/main/ui/Modal.ts
+++ src/main/ui/Modal.ts
@@ -1,3 +1,24 @@
 import UiElement from "./UiElement.js";
 
-export default class Modal extends UiElement {}
+/**
+ * Represents a pop-up box in the app for menus.
+ */
+export default class Modal extends UiElement {
+	public override readonly width: number;
+	public override readonly height: number;
+
+	/**
+	 * Creates a modal.
+	 *
+	 * @param width the modal's width in pixels
+	 * @param height the modal's height in pixels
+	 */
+	constructor(width: number, height: number) {
+		super(width, height);
+
+		this.width = width;
+		this.height = height;
+
+		this.getElement().classList.add("modal");
+	}
+}
--- src/main/ui/UiElement.ts
+++ src/main/ui/UiElement.ts
@@ -1,27 +1,56 @@
-import { create } from "../utils/Utils.js";
+import { create, get, px } from "../utils/Utils.js";
 
 /**
  * Represents a base `UiElement` object.
  */
 export default abstract class UiElement {
+	/**
+	 * The `UiElement`'s width in pixels.
+	 */
+	protected abstract readonly width: number | undefined;
+	/**
+	 * The `UiElement`'s height in pixels.
+	 */
+	protected abstract readonly height: number | undefined;
+
+	/**
+	 * This `UiElement`'s `HTMLElement`.
+	 */
 	private readonly element: HTMLElement;
+	/**
+	 * Base class of all `UiElement`s.
+	 */
 	private static readonly CLASS_NAME: "ui-element" = "ui-element";
+	/**
+	 * The default amount of padding (in pixels) of all `UiElement`s.
+	 */
+	private static readonly DEFAULT_PADDING: 20 = 20;
 
 	/**
 	 * Creates a base `UiElement` object.
+	 *
+	 * @param width the `UiElement`'s width in pixels
+	 * @param height the `UiElement`'s height in pixels
 	 */
-	constructor() {
+	constructor(width: number, height: number) {
 		const CLASS_NAME = UiElement.CLASS_NAME;
 		this.element = create({
 			name: "div",
 			classes: [CLASS_NAME],
 		});
 
+		this.element.css({
+			width: px(width),
+			height: px(height),
+		});
 		this.element.appendChild(
 			create({
 				name: "div",
 				classes: [`${CLASS_NAME}-inner`],
-			})
+			}).css({
+				width: px(width - UiElement.DEFAULT_PADDING),
+				height: px(height - UiElement.DEFAULT_PADDING),
+			}) as HTMLElement
 		);
 	}
 
@@ -30,5 +59,13 @@ export default abstract class UiElement {
 	 */
 	public getElement(): HTMLElement {
 		return this.element;
+	}
+
+	/**
+	 * Renders this `UiElement` to the actual screen. Should only be used for _finalized_ groups
+	 * of `UiElement`s or testing.
+	 */
+	public renderToGame(): void {
+		get("game")!.appendChild(this.element);
 	}
 }
