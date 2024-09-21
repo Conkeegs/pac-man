307328f036d10dec2c4c9f065c36ec83b332e9c1
Conkeegs <conkeegs@gmail.com>
2024-09-21 14:30:26 -0400

    base test class given comments and now properly logs to console upon fail() being called

--- tests/base/Base.ts
+++ tests/base/Base.ts
@@ -1,5 +1,34 @@
+import chalk from "chalk";
+import Logger from "./Logger.ts";
+
+/**
+ * The base test class.
+ */
 export default abstract class Test {
-	public run(): void {
-		// console.log({ propNames: Object.getOwnPropertyNames(this) });
+	/**
+	 * The name of this test class.
+	 */
+	protected abstract name: string;
+
+	/**
+	 * Logic to call when this test fails.
+	 *
+	 * @param message description of the testing failure
+	 */
+	public fail(message: string, stack?: string): void {
+		Logger.logFailure(`${message} in ${chalk.bold(this.getName())}`);
+
+		if (stack) {
+			Logger.log(stack);
+		}
+	}
+
+	/**
+	 * Get the name of the test.
+	 *
+	 * @returns the name of the test
+	 */
+	private getName(): string {
+		return this.name;
 	}
 }
