862ec3ee93cec92e62443ac2905f7b653f2bc380
Conkeegs <conkeegs@gmail.com>
2024-09-28 01:57:07 -0400

    collidablemanager test file created

---
+++ tests/board/boardobject/CollidableManagerTest.ts
@@ -0,0 +1,9 @@
+import CollidableManager from "../../../src/main/board/boardobject/CollidableManager.ts";
+import Test from "../../base/Base.ts";
+import { tests } from "../../base/Decorators.ts";
+
+/**
+ * Tests the functionality of a `BoardObject` instance.
+ */
+@tests(CollidableManager)
+export default class CollidableManagerTest extends Test {}
