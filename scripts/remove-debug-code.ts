efa6a8e71578475f6f9a625d31db6186568ac6b0
Conkeegs <conkeegs@gmail.com>
2024-10-12 00:04:28 -0400

    added script for removing debug flags in output javascript files

---
+++ scripts/remove-debug-code.ts
@@ -0,0 +1,53 @@
+import { readdirSync, readFileSync, statSync, writeFileSync } from "fs";
+import path from "path";
+
+const distDirectory: "dist" = "dist";
+const encoding: "utf-8" = "utf-8";
+const debugRegex: RegExp = /\/\/ #!DEBUG[\s\S]*?\/\/ #!END_DEBUG/gim;
+
+/**
+ * Replaces debug flags in an output-JavaScript file.
+ *
+ * @param jsFile the JavaScript file to remove debug code in
+ */
+function removeDebugCode(jsFile: string): void {
+	const jsFileContent = readFileSync(jsFile, {
+		encoding,
+	});
+
+	// replace debug code instances
+	writeFileSync(jsFile, jsFileContent.replace(debugRegex, "").trim(), {
+		encoding,
+	});
+}
+
+/**
+ * Reads directories located inside of the output dist files and its output JavaScript
+ * files and removes debug code.
+ *
+ * @param directoryPath directory of the "dist" folder or its child-directories
+ */
+function readDistFiles(directoryPath: string): void {
+	const files = readdirSync(directoryPath);
+
+	for (const file of files) {
+		const filePath = path.join(directoryPath, file);
+		const fileStats = statSync(filePath);
+
+		if (fileStats.isDirectory()) {
+			readDistFiles(filePath);
+
+			continue;
+		}
+
+		if (file.endsWith(".js")) {
+			removeDebugCode(filePath);
+		}
+	}
+}
+
+console.log("\nRemoving debug flags in output JavaScript files...");
+
+readDistFiles(distDirectory);
+
+console.log("Finished removing debug flags in output JavaScript files");
