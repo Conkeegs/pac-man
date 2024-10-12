0b488b8a66b39dec0b6ff5b4b8e28b3b81f737de
Conkeegs <conkeegs@gmail.com>
2024-10-12 00:56:27 -0400

    removed unused commented-out code

--- src/main/debugwindow/DebugWindow.ts
+++ src/main/debugwindow/DebugWindow.ts
@@ -1,23 +1,6 @@
 "use strict";
 
 import { App } from "../App.js";
-
-// positionSum = 0;
-// positionCount = 0;
-// frameSecondsSum = 0;
-// frameCount = 0; properties on character class for framerate
-
-// this.positionSum += this.speed! * millisToSeconds(timeStamp - lastAnimationTime);
-// this.positionCount++;
-// this.frameSecondsSum += millisToSeconds(timeStamp - lastAnimationTime); in if (lastAnimationTime) in Character class and "// this.frameCount++;" after setting lastAnimationTime
-
-// private moveCodeWords = {
-// 	[MovementDirection.LEFT]: "LEFT",
-// 	[MovementDirection.RIGHT]: "RIGHT",
-// 	[MovementDirection.UP]: "UP",
-// 	[MovementDirection.DOWN]: "DOWN",
-// };
-
 /**
  * Represents the debug window that shows up next to the board when the `DEBUG` app flag is set.
  * It logs important information about the game.
