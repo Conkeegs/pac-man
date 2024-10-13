655a3c62cd4cfe7e17cd5a508ccc29741b9e0734
Conkeegs <conkeegs@gmail.com>
2024-10-13 13:24:51 -0400

    imagesregistry image names fixed. pacman image names fixed to match animation frames

--- src/main/assets/ImageRegistry.ts
+++ src/main/assets/ImageRegistry.ts
@@ -5,15 +5,15 @@ import { getImageSrc } from "../utils/Utils.js";
  */
 export type IMAGE_LIST = {
 	"not-found": HTMLImageElement;
-	"pacman-0": HTMLImageElement;
-	"pacman-1-0": HTMLImageElement;
-	"pacman-1-1": HTMLImageElement;
-	"pacman-1-2": HTMLImageElement;
-	"pacman-1-3": HTMLImageElement;
+	"pacman-1": HTMLImageElement;
 	"pacman-2-0": HTMLImageElement;
 	"pacman-2-1": HTMLImageElement;
 	"pacman-2-2": HTMLImageElement;
 	"pacman-2-3": HTMLImageElement;
+	"pacman-3-0": HTMLImageElement;
+	"pacman-3-1": HTMLImageElement;
+	"pacman-3-2": HTMLImageElement;
+	"pacman-3-3": HTMLImageElement;
 	"blinky-1-0": HTMLImageElement;
 	"blinky-1-1": HTMLImageElement;
 	"blinky-1-2": HTMLImageElement;
@@ -57,47 +57,47 @@ export default class ImageRegistry {
 	 */
 	public static readonly IMAGE_LIST: IMAGE_LIST = {
 		"not-found": ImageRegistry.createImage("not-found"),
-		"pacman-0": ImageRegistry.createImage("pacman-0"),
-		"pacman-1-0": ImageRegistry.createImage("pacman-1-0"),
-		"pacman-1-1": ImageRegistry.createImage("pacman-1-1"),
-		"pacman-1-2": ImageRegistry.createImage("pacman-1-2"),
-		"pacman-1-3": ImageRegistry.createImage("pacman-1-3"),
+		"pacman-1": ImageRegistry.createImage("pacman-1"),
 		"pacman-2-0": ImageRegistry.createImage("pacman-2-0"),
 		"pacman-2-1": ImageRegistry.createImage("pacman-2-1"),
 		"pacman-2-2": ImageRegistry.createImage("pacman-2-2"),
 		"pacman-2-3": ImageRegistry.createImage("pacman-2-3"),
-		"blinky-1-0": ImageRegistry.createImage("blinky-0-0"),
-		"blinky-1-1": ImageRegistry.createImage("blinky-0-1"),
-		"blinky-1-2": ImageRegistry.createImage("blinky-0-2"),
-		"blinky-1-3": ImageRegistry.createImage("blinky-0-3"),
-		"blinky-2-0": ImageRegistry.createImage("blinky-1-0"),
-		"blinky-2-1": ImageRegistry.createImage("blinky-1-1"),
-		"blinky-2-2": ImageRegistry.createImage("blinky-1-2"),
-		"blinky-2-3": ImageRegistry.createImage("blinky-1-3"),
-		"inky-1-0": ImageRegistry.createImage("inky-0-0"),
-		"inky-1-1": ImageRegistry.createImage("inky-0-1"),
-		"inky-1-2": ImageRegistry.createImage("inky-0-2"),
-		"inky-1-3": ImageRegistry.createImage("inky-0-3"),
-		"inky-2-0": ImageRegistry.createImage("inky-1-0"),
-		"inky-2-1": ImageRegistry.createImage("inky-1-1"),
-		"inky-2-2": ImageRegistry.createImage("inky-1-2"),
-		"inky-2-3": ImageRegistry.createImage("inky-1-3"),
-		"pinky-1-0": ImageRegistry.createImage("pinky-0-0"),
-		"pinky-1-1": ImageRegistry.createImage("pinky-0-1"),
-		"pinky-1-2": ImageRegistry.createImage("pinky-0-2"),
-		"pinky-1-3": ImageRegistry.createImage("pinky-0-3"),
-		"pinky-2-0": ImageRegistry.createImage("pinky-1-0"),
-		"pinky-2-1": ImageRegistry.createImage("pinky-1-1"),
-		"pinky-2-2": ImageRegistry.createImage("pinky-1-2"),
-		"pinky-2-3": ImageRegistry.createImage("pinky-1-3"),
-		"clyde-1-0": ImageRegistry.createImage("clyde-0-0"),
-		"clyde-1-1": ImageRegistry.createImage("clyde-0-1"),
-		"clyde-1-2": ImageRegistry.createImage("clyde-0-2"),
-		"clyde-1-3": ImageRegistry.createImage("clyde-0-3"),
-		"clyde-2-0": ImageRegistry.createImage("clyde-1-0"),
-		"clyde-2-1": ImageRegistry.createImage("clyde-1-1"),
-		"clyde-2-2": ImageRegistry.createImage("clyde-1-2"),
-		"clyde-2-3": ImageRegistry.createImage("clyde-1-3"),
+		"pacman-3-0": ImageRegistry.createImage("pacman-3-0"),
+		"pacman-3-1": ImageRegistry.createImage("pacman-3-1"),
+		"pacman-3-2": ImageRegistry.createImage("pacman-3-2"),
+		"pacman-3-3": ImageRegistry.createImage("pacman-3-3"),
+		"blinky-1-0": ImageRegistry.createImage("blinky-1-0"),
+		"blinky-1-1": ImageRegistry.createImage("blinky-1-1"),
+		"blinky-1-2": ImageRegistry.createImage("blinky-1-2"),
+		"blinky-1-3": ImageRegistry.createImage("blinky-1-3"),
+		"blinky-2-0": ImageRegistry.createImage("blinky-2-0"),
+		"blinky-2-1": ImageRegistry.createImage("blinky-2-1"),
+		"blinky-2-2": ImageRegistry.createImage("blinky-2-2"),
+		"blinky-2-3": ImageRegistry.createImage("blinky-2-3"),
+		"inky-1-0": ImageRegistry.createImage("inky-1-0"),
+		"inky-1-1": ImageRegistry.createImage("inky-1-1"),
+		"inky-1-2": ImageRegistry.createImage("inky-1-2"),
+		"inky-1-3": ImageRegistry.createImage("inky-1-3"),
+		"inky-2-0": ImageRegistry.createImage("inky-2-0"),
+		"inky-2-1": ImageRegistry.createImage("inky-2-1"),
+		"inky-2-2": ImageRegistry.createImage("inky-2-2"),
+		"inky-2-3": ImageRegistry.createImage("inky-2-3"),
+		"pinky-1-0": ImageRegistry.createImage("pinky-1-0"),
+		"pinky-1-1": ImageRegistry.createImage("pinky-1-1"),
+		"pinky-1-2": ImageRegistry.createImage("pinky-1-2"),
+		"pinky-1-3": ImageRegistry.createImage("pinky-1-3"),
+		"pinky-2-0": ImageRegistry.createImage("pinky-2-0"),
+		"pinky-2-1": ImageRegistry.createImage("pinky-2-1"),
+		"pinky-2-2": ImageRegistry.createImage("pinky-2-2"),
+		"pinky-2-3": ImageRegistry.createImage("pinky-2-3"),
+		"clyde-1-0": ImageRegistry.createImage("clyde-1-0"),
+		"clyde-1-1": ImageRegistry.createImage("clyde-1-1"),
+		"clyde-1-2": ImageRegistry.createImage("clyde-1-2"),
+		"clyde-1-3": ImageRegistry.createImage("clyde-1-3"),
+		"clyde-2-0": ImageRegistry.createImage("clyde-2-0"),
+		"clyde-2-1": ImageRegistry.createImage("clyde-2-1"),
+		"clyde-2-2": ImageRegistry.createImage("clyde-2-2"),
+		"clyde-2-3": ImageRegistry.createImage("clyde-2-3"),
 	};
 
 	/**
