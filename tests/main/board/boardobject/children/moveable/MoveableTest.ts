14a693dc92ca9200662cec1ce6e9190c41ec0b52
Conkeegs <conkeegs@gmail.com>
2024-10-16 19:27:38 -0400

    initial moveable test file

---
+++ tests/main/board/boardobject/children/moveable/MoveableTest.ts
@@ -0,0 +1,9 @@
+import Moveable from "../../../../../../src/main/board/boardobject/children/moveable/Moveable.js";
+import Test from "../../../../../base/Base.js";
+import { tests } from "../../../../../base/Decorators.js";
+
+/**
+ * Tests functionality of `Moveable` instances.
+ */
+@tests(Moveable)
+export default class MoveableTest extends Test {}
