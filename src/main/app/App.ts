10d6479f5b8606360edcec7ccbe9cd50b578b869
Conkeegs <conkeegs@gmail.com>
2026-05-17 16:56:45 -0500

    collision ccd uses speed in calculation rather than distancePerTick

--- src/main/app/App.ts
+++ src/main/app/App.ts
@@ -681,7 +681,7 @@ export class App {
 
 		// if the collidable doesn't move a greater distance than its collision box each tick,
 		// we can stop here
-		if (!(collidable.getDistancePerTick() >= collisionBox.getWidth())) {
+		if (!(collidable.getSpeed() >= collisionBox.getWidth())) {
 			return;
 		}
 
